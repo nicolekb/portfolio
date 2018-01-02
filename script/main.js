8006badd81963a264bd01fa177f6c12801631f5a
nicolekb <nicolekbuloran@gmail.com>
2018-01-02 11:25:48 -0700

    Added header, one work item

---
+++ script/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.-items').slick({
+  	dots: true,
+	  infinite: true,
+	  speed: 300,
+	  slidesToShow: 1,
+	  adaptiveHeight: true
+  });
+});
+
+
+  console.log("hello");
