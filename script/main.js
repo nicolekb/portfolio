ffffa28506a365be442d6d352ba5f9e8492c4007
nicolekb <nicolekbuloran@gmail.com>
2018-01-08 12:44:21 -0700

    Fixed social media icon, Fixed work section styles, linked social media icon, created smooth scroll

--- script/main.js
+++ script/main.js
@@ -1,3 +1,9 @@
+//*******//
+//*	Template Name: main.js
+//* Desc: Website functionaity
+//*******//
+
+//***** Slick *****//
 $(document).ready(function($) {
 	
 	$('.work-items').slick({
@@ -6,8 +12,24 @@ $(document).ready(function($) {
 	  speed: 300,
 	  slidesToShow: 1,
 	  adaptiveHeight: true,
-	});
+	  fade: true,
+	  cssEase: 'linear',
+	  responsive: [{
+            breakpoint: 600,
+            settings: {
+                arrows: true,
+            }
 
-	
-		
+      }],
+
+	});
+});
+
+//***** Scroll *****//
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
 });
